1276b867dcf0096378fa3fa848d688d2198b85f5
wzx <1327235274@qq.com>
2019-07-15 19:38:40 +0800

    resources

---
+++ src/main/java/com/qf/pojo/Student.java
@@ -0,0 +1,12 @@
+package com.qf.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private int sid;
+    private String sname;
+    private String username;
+    private String password;
+    private int age;
+    private Date sdate;
+}
