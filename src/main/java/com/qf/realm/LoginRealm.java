d04af5b3e80393ed7efb860922c43315c51cfa5f
wzx <1327235274@qq.com>
2019-07-16 12:05:22 +0800

    wang

---
+++ src/main/java/com/qf/realm/LoginRealm.java
@@ -0,0 +1,32 @@
+package com.qf.realm;
+
+import com.qf.service.LoginService;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.crypto.hash.Md5Hash;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LoginRealm extends AuthorizingRealm {
+    @Autowired
+    private LoginService loginService;
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        String principal = (String) authenticationToken.getPrincipal();
+        String[] split = principal.split(":");
+        String password = loginService.getPassword(principal);
+        SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(split[0], password, "LoginRealm");
+
+
+        return simpleAuthenticationInfo;
+    }
+}
