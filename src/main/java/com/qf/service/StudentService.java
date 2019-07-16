22ef4d0dd99aea4e1417c5d27f8ad162cf9d8efa
yanchuanhao <1198632539@qq.com>
2019-07-16 09:53:20 +0800

    212313513

---
+++ src/main/java/com/qf/service/StudentService.java
@@ -0,0 +1,9 @@
+package com.qf.service;
+
+import com.qf.pojo.Student;
+
+public interface StudentService {
+
+    public int updateStudentInfo(Student student);
+
+}
--- src/main/java/com/qf/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.qf.service;
-
-public class UserService {
-}
